d0446cdd23d3c9b11ae6bcb7994e58874be2d394
Craftless <javier.tzh@gmail.com>
2022-04-08 17:40:52 +0800

    gitignore taking effect

--- build/tmp/expandedArchives/forge-1.16.1-32.0.63_mapped_snapshot_20200707-1.16.1-sources.jar_141e88cf380bd16536c87695662a168d/net/minecraft/util/TickRangeConverter.java
+++
@@ -1,7 +0,0 @@
-package net.minecraft.util;
-
-public class TickRangeConverter {
-   public static RangedInteger convertRange(int minIn, int maxIn) {
-      return new RangedInteger(minIn * 20, maxIn * 20);
-   }
-}
